397ea7b38037f14d9a5ee99106151a1c497eb0db
Yuvashree <yuvashree.k007@gmail.com>
2025-12-09 08:51:47 +0530

    Day6

---
+++ Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+const Header=()=>
+{
+    let des={backgroundColor:"black",color:"white"};
+    return (
+<div className="p1">
+    <h1 style={des}>Tiger</h1>
+
+    <p >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam hic est corrupti dolor 
+        provident culpa odio, blanditiis sed cumque iusto saepe distinctio ducimus rem magnam animi 
+        sit exercitationem sint repellendus!</p>
+
+</div>
+)
+}
+export default Header
